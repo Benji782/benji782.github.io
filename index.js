79fd1cde4d98e8f5a468b0998bf918023902cc6e
Benji782 <benjaminnoel782@gmail.com>
2026-02-19 12:31:58 -0800

    Base Functionality

--- index.js
+++ index.js
@@ -1,43 +1,94 @@
-function Card(title, medium, sourceIMG){
+
+//Card Constructor
+function Card(title, medium, sourceIMG, description){
     this.title = title;
     this.medium = medium;
     this.sourceIMG = sourceIMG;
+    this.description = description;
 }
 
-const example1 = new Card("exampleTitle", "website", "")
-const example2 = new Card("exampleTwo", "design", "")
-const example3 = new Card("exampleThree", "design", "")
-var allCards = [example1, example2, example3]
 
 
-const designButton = document.getElementById("design")
-const websitesButton = document.getElementById("websites")
 
-designButton.addEventListener("click", function(){
-    workDisplay("design")
+
+
+
+const project1 = new Card("Butterfly", "design", "images/butterfly.png","A simple illustration of butterflies cresting on a sunset")
+const project2 = new Card("Snapped", "design", "images/snapped.jpg", "A physical ink drawing of a resting figure in wrapped bandages")
+const project3 = new Card("Support", "design", "images/support.png", "A peach tree sprouts from hands of stone")
+const project4 = new Card("The Nexus", "website", "images/theNexus.png", "A mock website for an assignment designed to emulate the brand identity of Meow Wolf")
+const project5 = new Card("Sustainable Food Systems", "website", "images/foodSystems.png", "Professional work commissioned by the head of Sustainable Food at CSUSB")
+
+//REMEMBER TO ADD THE PROJECT AS A CONST AND ADD TO THE ARRAY
+var allCards = [project1, project2, project3, project4, project5]
+
+
+
+
+
+
+//Search on load
+window.addEventListener("DOMContentLoaded",function(){
+    const designButton = document.getElementById("design")
+    const websitesButton = document.getElementById("websites")
+        designButton.addEventListener("click", function(){
+            workDisplay("design")})
+        websitesButton.addEventListener("click", function(){
+            workDisplay("website")})
+    
+    
+    const params = new URLSearchParams(window.location.search);
+    const category = params.get("category");
+    if (category === "design") workDisplay("design");
+    if (category === "websites") workDisplay("website")
+
 })
-websitesButton.addEventListener("click", function(){
-    workDisplay("website")
-})
 
 
 
+//THE FILTER. Shows items on click
 function workDisplay(mediumType){
-    let workContainer = document.getElementById("workContainer")
+    let workContainer = document.getElementById("workContainer");
+    let popUp = document.getElementById("popUp");
     workContainer.innerHTML = "";
-    for (var i = 0; i < allCards.length; i++){
+    
+    for (let i = 0; i < allCards.length; i++){
         if (allCards[i].medium === mediumType){
-            var cardElement = document.createElement("div")
-            var title = document.createElement("h3");
+            let cardData = allCards;
+            let cardElement = document.createElement("div")
+            let title = document.createElement("h3");
             title.textContent = allCards[i].title;
 
-            var image = document.createElement("img");
+            let image = document.createElement("img");
             image.src = allCards[i].sourceIMG;
             image.width = 150;
 
             cardElement.appendChild(title);
             cardElement.appendChild(image);
             workContainer.appendChild(cardElement);
+
+            cardElement.addEventListener("click", function(){
+                popUp.innerHTML = "";
+                popUp.classList.remove("hidden")
+                let bigTitle = document.createElement("h2");
+                    bigTitle.textContent = allCards[i].title
+                let bigImage = document.createElement("img");
+                    bigImage.src = allCards[i].sourceIMG;
+                let description = document.createElement("p");
+                    description.textContent = allCards[i].description;
+                let backBtn = document.createElement("a");
+                    backBtn.href = "#"
+                    backBtn.textContent = "BACK";
+                backBtn.addEventListener("click", function(e){
+                    e.preventDefault();
+                    popUp.classList.add("hidden")
+                })
+                popUp.appendChild(bigTitle);
+                popUp.appendChild(bigImage);
+                popUp.appendChild(description);
+                popUp.appendChild(backBtn);
+
+            })
         }
     }
 }
